5effb193ff44655af45704f319eaf77ab9577450
Kither12 <developerkither2509@gmail.com>
2023-01-01 19:39:25 +0700

    Add EulerTotient

---
+++ Math/EulerTotient.cpp
@@ -0,0 +1,26 @@
+long long eulerPhi(long long n) { // = n (1-1/p1) ... (1-1/pn)
+    if (n == 0) return 0;
+    long long ans = n;
+    for (int x = 2; x*x <= n; ++x) {
+        if (n % x == 0) {
+            ans -= ans / x;
+            while (n % x == 0) n /= x;
+        }
+    }
+    if (n > 1) ans -= ans / n;
+    return ans;
+}
+
+vector<int> phi_n(int n) {
+    vector<int> phi(n + 1);
+    for (int i = 0; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+    return phi;
+}
