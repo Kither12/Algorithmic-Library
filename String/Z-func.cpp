69404a815d86b215e74ffdd39bdb51fa1b770b06
Kither12 <61571510+Kither12@users.noreply.github.com>
2023-01-14 12:32:01 +0700

    fixed bug zfunc

--- String/Z-func.cpp
+++ String/Z-func.cpp
@@ -1,7 +1,6 @@
 vector<int> zfunc(string s) {
     int n = (int) s.length();
     vector<int> z(n);
-    z[0] = n;
     for (int i = 1, l = 0, r = 0; i < n; ++i) {
         if (i <= r)
             z[i] = min(r - i + 1, z[i - l]);
